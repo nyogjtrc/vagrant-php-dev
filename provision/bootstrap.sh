19a3c3c73ecb035c3ce36794dd005b916a36d1f7
nyo <billy31210@gmail.com>
2015-06-07 18:59:07 +0800

    add bootstrap.sh

---
+++ provision/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#apt-get update
+
+# git
+apt-get -y install git git-doc
+
+# nginx
+apt-get -y install nginx
+
+# php5
+apt-get -y install php5-fpm php5-cli php5-dev php-pear php5-mysql php5-curl php5-mcrypt
+
+# mysql
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+apt-get -y install mysql-server
+
+# redis
+apt-get -y install redis-server
+
